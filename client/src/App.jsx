2b3ce8d94660f0d14f18b45fc4170116d7cff803
Shubh Sardana <52607235+shubhsardana29@users.noreply.github.com>
2023-05-10 03:07:59 +0530

    updated colorscheme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import Header from "./components/Header";
-import Form from "@/components/Form"
+import Form from "@/components/Form";
 
 function App() {
   return (
-    <div className="app bg-opaque-black">
+    <div className="app bg-black">
       <Header />
       <div className="flex justify-center items-center h-screen">
-        <h1 className="font-playfair text-5xl font-bold text-green">
+        <h1 className="font-playfair text-5xl font-bold text-white">
           Making Readmes Just Got Simpler
         </h1>
       </div>
--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -33,71 +33,73 @@ function FormComponent() {
   const [markdown, setMarkdown] = useState('');
 
   return (
-    <div className="flex flex-row">
-      <form className="w-1/2 p-4">
-        <div className="mb-4">
-          <label className="block text-gray-700 font-bold mb-2" htmlFor="name">
-            Github UserId
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="name"
-            type="text"
-            placeholder="@username"
-            value={name}
-            onChange={handleNameChange}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block font-bold mb-2" htmlFor="learning">
-            Currently Learning
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="learning"
-            type="text"
-            placeholder="ReactJS"
-            value={learning}
-            onChange={handleLearningChange}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block font-bold mb-2" htmlFor="learning">
-            Talks about
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="learning"
-            type="text"
-            placeholder="ReactJS"
-            value={talksAbout}
-            onChange={handleTalksAboutChange}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block font-bold mb-2" htmlFor="other-details">
-            Other Details
-          </label>
-          <textarea
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="other-details"
-            placeholder="Enter additional details here..."
-            value={otherDetails}
-            onChange={handleOtherDetailsChange}
-          />
-        </div>
-        <button
-          className="bg-blue hover:bg-deep-blue text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          type="submit"
-          onClick={handleSubmit}
-        >
-          Generate Readme
-        </button>
-      </form>
-      <div className="w-1/2 p-4 bg-opaque-black text-white">
-        <ReactMarkdown>{markdown}</ReactMarkdown>
-      </div>
+<div className="flex flex-row">
+  <form className="w-1/2 p-4 bg-off-white">
+    <div className="mb-4">
+      <label className="block text-gray-700 font-bold mb-2" htmlFor="name">
+        Github UserId
+      </label>
+      <input
+        className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline bg-white text-dark-grey"
+        id="name"
+        type="text"
+        placeholder="@username"
+        value={name}
+        onChange={handleNameChange}
+      />
     </div>
+    <div className="mb-4">
+      <label className="block font-bold mb-2" htmlFor="learning">
+        Currently Learning
+      </label>
+      <input
+        className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline bg-white text-dark-grey"
+        id="learning"
+        type="text"
+        placeholder="ReactJS"
+        value={learning}
+        onChange={handleLearningChange}
+      />
+    </div>
+    <div className="mb-4">
+      <label className="block font-bold mb-2" htmlFor="learning">
+        Talks about
+      </label>
+      <input
+        className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline bg-white text-dark-grey"
+        id="learning"
+        type="text"
+        placeholder="ReactJS"
+        value={talksAbout}
+        onChange={handleTalksAboutChange}
+      />
+    </div>
+    <div className="mb-4">
+      <label className="block font-bold mb-2" htmlFor="other-details">
+        Other Details
+      </label>
+      <textarea
+        className="shadow appearance-none border rounded w-full py-2 px-3 text-dark-grey leading-tight focus:outline-none focus:shadow-outline bg-white"
+        id="other-details"
+        placeholder="Enter additional details here..."
+        value={otherDetails}
+        onChange={handleOtherDetailsChange}
+      />
+    </div>
+    <button
+      className="bg-black hover:bg-grey text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      type="submit"
+      onClick={handleSubmit}
+    >
+      Generate Readme
+    </button>
+  </form>
+  <div className="w-1/2 p-4 bg-dark-grey text-white">
+    <ReactMarkdown>{markdown}</ReactMarkdown>
+  </div>
+</div>
+
+
   );
 }
 
